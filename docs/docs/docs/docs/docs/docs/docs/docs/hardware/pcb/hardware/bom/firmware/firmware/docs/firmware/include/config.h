ecbfe537eacd299d09e85e2537b4bc8c77ea68a7
ag7056 <alexisg7056@gmail.com>
2026-04-13 14:15:54 -0500

    Create config.h

---
+++ docs/docs/docs/docs/docs/docs/docs/docs/hardware/pcb/hardware/bom/firmware/firmware/docs/firmware/include/config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define TEMP_LOW_THRESHOLD_C   30
+#define TEMP_MID_THRESHOLD_C   40
+#define TEMP_HIGH_THRESHOLD_C  50
+
+#define FAN_PWM_MIN            20
+#define FAN_PWM_MED            55
+#define FAN_PWM_HIGH           90
+
+#endif
